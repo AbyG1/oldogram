5e7b556b20540269be8075904910afbc024a7929
AbyG1 <abygeorge133@gmail.com>
2024-01-27 00:11:21 +0530

    add some modifications

--- index.js
+++ index.js
@@ -1,30 +1,69 @@
-const posts = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 4
-    },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
-    }
-]
+// const posts = [
 
+//     {
+//         name: "Gustave Courbet",
+//         username: "gus1819",
+//         location: "Ornans, France",
+//         avatar: "images/avatar-courbet.jpg",
+//         post: "images/post-courbet.jpg",
+//         comment: "i'm feelin a bit stressed tbh",
+//         likes: 4
+//     },
+//         {
+//         name: "Joseph Ducreux",
+//         username: "jd1735",
+//         location: "Paris, France",
+//         avatar: "images/avatar-ducreux.jpg",
+//         post: "images/post-ducreux.jpg",
+//         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+//         likes: 152
+//     }
+// ]
+
+
+// for( let i of posts){
+//     let mainContainer = document.getElementsByTagName('main')
+  
+
+//     let avatarPic = document.createElement('img')
+
+//     let avatarContainer = document.createElement('div')
+
+//     let avatarName = document.createElement('p')
+//     let avatarLocation = document.createElement('p')
+
+//     let postContainer = document.createElement('div')
+
+//     let oldoPost = document.createElement('img')
+   
+//     // let likeCount = document.CreateElementById('span')
+
+//     avatarPic.setAttribute('src', i.avatar)
+//     avatarName.textContent = i.name
+//     avatarLocation.textContent = i.location
+//     avatarContainer.classList.add('profile-deatails')
+
+
+//     oldoPost.setAttribute('src',i.post)
+//     // likeCount.textContent = i.likes
+
+//     avatarPic.classList.add('avatar-img')
+//     avatarName.classList.add('name')
+//     // avatarLocation.classList.add('')
+
+//     oldoPost.classList.add('post-img')
+
+
+
+//     avatarContainer.appendChild(avatarPic)
+//     avatarContainer.appendChild(avatarName)
+//     avatarContainer.appendChild(avatarLocation)
+//     postContainer.appendChild(oldoPost)
+//     mainContainer[0].appendChild(avatarContainer)
+//     mainContainer[0].appendChild(postContainer)
+   
+
+
+//     // sectionTwo.appendChild('likeCount')
+
+// }
