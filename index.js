ce2532cd8efb8797bd311f5fbe874d3d8106fcbf
AbyG1 <abygeorge133@gmail.com>
2024-05-19 08:16:00 +0530

    Add like feature

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+
 const posts = [
     {
         name: "Vincent van Gogh",
@@ -6,7 +8,9 @@ const posts = [
         avatar: "/images/avatar-vangogh.jpg",
         post: "/images/post-vangogh.jpg",
         comment: " just took a few mushrooms lol",
-        likes:21
+        likes:21,
+        isLiked:false,
+        uuid: uuidv4()
     },
 
     {
@@ -16,7 +20,9 @@ const posts = [
         avatar: "images/avatar-courbet.jpg",
         post: "images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
-        likes: 4
+        likes: 4,
+        isLiked:false,
+        uuid: uuidv4()
     },
         {
         name: "Joseph Ducreux",
@@ -25,21 +31,50 @@ const posts = [
         avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
+        likes: 152,
+        isLiked:false,
+        uuid: uuidv4()
     }
 ]
 
 
-
 let feedContainer = document.getElementById("main")
 
+feedContainer.addEventListener(('click'),(e) => {
+    if((e.target.dataset.like)){
+        handleLikes(e.target.dataset.like)
+    }[0]
+    
+})
+
+
+function handleLikes(postID){
+
+    const targetFeed = posts.filter((post) => {
+        return post.uuid === postID
+    } )[0]
+
+
+    
+    if(targetFeed.isLiked){
+        
+        targetFeed.likes--
+    } else{
+       
+        targetFeed.likes++
+    }
+    targetFeed.isLiked = !(targetFeed.isLiked) 
+    getPosts()
+    
+}
 
 
 
 function getPosts(){
 
     let postContents = ''
-    posts.forEach(element => {
+    posts.forEach((element,index) => {
+
         postContents += `
         
                     <section>
@@ -58,7 +93,7 @@ function getPosts(){
                         <img id='oldagram-post' src="${element.post}" class="post-img" alt="Selfie painting of vangogh">
                         <div class="reactions">
                             <div class="engagements">
-                                <img src="./images/icon-heart.png" alt="like icon">
+                                <img src="./images/icon-heart.png" id="like${index}" data-like=${element.uuid} alt="like icon">
                                 <img src="./images/icon-comment.png" alt="comment icon">
                                 <img src="./images/icon-dm.png" alt="share icon">
                             </div>
